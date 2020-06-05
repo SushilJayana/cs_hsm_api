75e48253b791b9d39de11d8eb39cd807728ccb2f
Sushil Jayana <sushil.sth.47@gmail.com>
2020-06-05 07:48:56 +0545

    routes

--- routes/api.php
+++ routes/api.php
@@ -22,17 +22,16 @@ Route::resource('user', 'Api\v1\Application\UserController');
 
 Route::post('/login', 'Api\v1\Shared\LoginController@login')->name('login');
 
-Route::group(['middleware' => ['role:administrator']], function () {
+Route::group(['middleware' => ['auth:api','role:administrator']], function () {
     Route::get('/admin', function () {
         return "admin";
-    })->middleware('auth:api');
+    });
 });
 
-Route::group(['middleware' => ['role:faculty']], function () {
+Route::group(['middleware' => ['auth:api','role:faculty']], function () {
     Route::get('/faculty', function () {
         return "faculty";
-    })->middleware('auth:api');
+    });
 });
-
 
 
