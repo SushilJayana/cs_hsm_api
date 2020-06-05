db92b8fb644faf693019e2a562285f10d7674b2e
Sushil Jayana <sushil.sth.47@gmail.com>
2020-06-05 07:50:07 +0545

    user crud

--- routes/api.php
+++ routes/api.php
@@ -1,28 +1,14 @@
 <?php
 
-use Illuminate\Http\Request;
 use Illuminate\Support\Facades\Route;
 
-/*
-|--------------------------------------------------------------------------
-| API Routes
-|--------------------------------------------------------------------------
-|
-| Here is where you can register API routes for your application. These
-| routes are loaded by the RouteServiceProvider within a group which
-| is assigned the "api" middleware group. Enjoy building your API!
-|
-*/
-
-//Route::middleware('auth:api')->get('/user', function (Request $request) {
-//    return $request->user();
-//});
 
 Route::resource('user', 'Api\v1\Application\UserController');
 
 Route::post('/login', 'Api\v1\Shared\LoginController@login')->name('login');
 
 Route::group(['middleware' => ['auth:api','role:administrator']], function () {
+//    Route::resource('user', 'Api\v1\Application\UserController');
     Route::get('/admin', function () {
         return "admin";
     });
