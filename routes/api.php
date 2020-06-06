449263854bb87d722483ad349fce13525796be80
Sushil Jayana <sushil.sth.47@gmail.com>
2020-06-06 11:21:43 +0545

    routes

--- routes/api.php
+++ routes/api.php
@@ -2,19 +2,16 @@
 
 use Illuminate\Support\Facades\Route;
 
-Route::post('user','Api\v1\Application\UserController@store');
-
-
 Route::post('/login', 'Api\v1\Shared\LoginController@login')->name('login');
 
-Route::group(['middleware' => ['auth:api','role:administrator']], function () {
-//    Route::resource('user', 'Api\v1\Application\UserController');
+Route::group(['middleware' => ['auth:api', 'role:administrator']], function () {
+    Route::resource('user', 'Api\v1\Application\UserController');
     Route::get('/admin', function () {
         return "admin";
     });
 });
 
-Route::group(['middleware' => ['auth:api','role:faculty']], function () {
+Route::group(['middleware' => ['auth:api', 'role:faculty']], function () {
     Route::get('/faculty', function () {
         return "faculty";
     });
